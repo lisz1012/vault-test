6e85cdbc02349ef5bc92dccaf50fa234e491be37
lisz1012 <lisz1012@163.com>
2023-09-11 01:35:09 -0700

    Initial commit

---
+++ src/main/java/com/example/vaulttest/VaultTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaulttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultTestApplication.class, args);
+	}
+
+}
